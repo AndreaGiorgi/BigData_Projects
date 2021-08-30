a400c5eaf5766019981b534422a46fc4e197462c
Andrea Giorgi <andrea_giorgi@outlook.it>
2021-08-30 13:09:45 +0200

    [Clubhouse[ Fix

--- Project 2/src/clubhouse/clubhouse_pipeline.py
+++ Project 2/src/clubhouse/clubhouse_pipeline.py
@@ -4,5 +4,7 @@ from dataset_preprocessing import preprocessing_pipeline
 def pipeline():
     loading_pipeline()
     preprocessing_pipeline()
-    
+
+if __name__ == '__main__':
+    pipeline()
     
